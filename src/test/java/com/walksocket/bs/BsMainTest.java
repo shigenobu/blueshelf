ed3b6eb627c7500261b0d8f1c9cda0a08dd91cea
Shigenobu Furuta <shigenobu@walksocket.com>
2018-10-23 14:27:40 +0900

    Add travis CI.

--- src/test/java/com/walksocket/bs/BsMainTest.java
+++ src/test/java/com/walksocket/bs/BsMainTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.List;
+import java.util.TimeZone;
 
 import static org.junit.Assert.*;
 
@@ -70,6 +71,7 @@ public class BsMainTest {
   @BeforeClass
   public static void beforeClass() {
     BsLogger.setVerbose(true);
+    BsDate.setTimeZone(TimeZone.getTimeZone("Asia/Tokyo"));
   }
 
 //  @Test
@@ -213,8 +215,77 @@ public class BsMainTest {
   }
 
   @Test
-  public void testClientSingleAndServerMulti() {
+  public void testClientSingleAndServerMulti() throws BsLocal.BsLocalException, BsExecutorServer.BsExecutorServerException {
+    // for server
+    List<BsLocal> locals4server = Arrays.asList(
+        new BsLocal("0.0.0.0", 8711),
+        new BsLocal("0.0.0.0", 8712),
+        new BsLocal("0.0.0.0", 8713)
+    );
+    BsExecutorServer executorServer = new BsExecutorServer(new BsCallback() {
+      @Override
+      public void incoming(BsRemote remote, byte[] message) {
+        System.out.println("incoming server :" + new String(message));
+        try {
+          remote.send("hello from server by incoming".getBytes(StandardCharsets.UTF_8));
+        } catch (BsRemote.BsSendException e) {
+          e.printStackTrace();
+        }
+      }
+    }, locals4server);
+    executorServer.start();
 
+    // for client
+    for (int port : Arrays.asList(8711, 8712, 8713)) {
+      for (int i = 0; i < 2; i++) {
+        int clientPort = port + 10000 + i * 1000;
+        new Thread(() -> {
+          BsLocal local4client = null;
+          try {
+            local4client = new BsLocal("0.0.0.0", clientPort);
+          } catch (BsLocal.BsLocalException e) {
+            e.printStackTrace();
+          }
+          BsRemote remote4client = new BsRemote("127.0.0.1", port, local4client.getSendChannel());
+          BsExecutorClient executorClient = new BsExecutorClient(new BsCallback() {
+            @Override
+            public void incoming(BsRemote remote, byte[] message) {
+              System.out.println("incoming client :" + new String(message));
+              try {
+                remote.send("hello from client by incoming".getBytes(StandardCharsets.UTF_8));
+              } catch (BsRemote.BsSendException e) {
+                e.printStackTrace();
+              }
+            }
+          }, local4client, remote4client);
+          try {
+            executorClient.start();
+          } catch (BsExecutorClient.BsExecutorClientException e) {
+            e.printStackTrace();
+          }
+          try {
+            remote4client.send(("hello from client 1 port " + clientPort).getBytes(StandardCharsets.UTF_8));
+            remote4client.send(("hello from client 2 port " + clientPort).getBytes(StandardCharsets.UTF_8));
+            Thread.sleep(2000);
+          } catch (BsRemote.BsSendException e) {
+            e.printStackTrace();
+          } catch (InterruptedException e) {
+            e.printStackTrace();
+          }
+          executorClient.shutdown();
+
+        }).start();
+      }
+
+    }
+
+    try {
+      Thread.sleep(500);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+
+    executorServer.shutdown();
   }
 
 }
